608778634ac1f11c292e572c15c0a60075da227d
felipe quintana <felipe.q1989@gmail.com>
2018-07-16 20:07:27 -0500

    car new

---
+++ app/controllers/cars_controller.rb
@@ -0,0 +1,43 @@
+class CarsController < ApplicationController
+  def index
+    @cars = Car.where(user_id: current_user.id)
+  end
+
+  def new
+    @user = current_user
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+    @car.user = current_user
+
+    if @car.save
+      redirect_to user_cars_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_car
+    @car = Car.find(params[:id])
+  end
+
+  def car_params
+    params.require(:car).permit!
+  end
+end
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,5 +1,5 @@
 class Car < ApplicationRecord
   belongs_to :user
-  has_many :appointments, dependant: :destroy
+  has_many :appointments, dependent: :destroy
 
 end
